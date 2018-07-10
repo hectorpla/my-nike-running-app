cbe6029b1ca155aef3ad491e65d7847720cc4808
Zhanpeng Liang <hectorilles@gmail.com>
2018-07-09 19:41:39 -0700

    fixes for Spring Cloud

--- running-location-updater/src/main/java/demo/RunningLocationUpdaterApplication.java
+++ running-location-updater/src/main/java/demo/RunningLocationUpdaterApplication.java
@@ -2,12 +2,14 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by hectorlueng on 5/14/18.
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RunningLocationUpdaterApplication {
     public static void main(String[] args) {
         SpringApplication.run(RunningLocationUpdaterApplication.class, args);
